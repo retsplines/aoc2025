8ba7497184e429d525c740ae0f18649c91cdfdbd
Ret <folf@retsplin.es>
2025-12-06 00:05:53 +0000

    Add: Day 5 part 2

--- day05/src/part2.rs
+++ day05/src/part2.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 struct Range {
     start: u64,
     end: u64
@@ -18,6 +18,39 @@ impl FromStr for Range {
             start: start.parse().expect("Bad range start"),
             end: end[1..].parse().expect("Bad range end")
         })
+    }
+}
+
+/// Merge two ranges into one, if possible
+fn merge(left: &Range, right: &Range) -> Option<Range> {
+
+    // If the left range starts before the right range starts
+    // [left--------?
+    //     [right---?
+    if left.start <= right.start {
+
+        // Right entirely contained by left
+        // [----left----]
+        //    [-right-]
+        if right.end <= left.end {
+            Some(left.clone())
+
+        // Left ends before right starts, so no intersection at all
+        // [---left---]
+        //              [---right---
+        } else if left.end < right.start {
+            None
+
+        // Intersecting
+        // [---left---]
+        //    [---right---]
+        } else {
+            Some(Range { start: left.start, end: right.end })
+        }
+
+    } else {
+        // Try the other way around too
+        merge(right, left)
     }
 }
 
@@ -43,9 +76,48 @@ fn main() {
             break;
         }
 
-        let range: Range = line.parse().expect("Bad range");
-
-        // TODO: Work out if the range extends any existing range, or add it to ranges
-
+        ranges.push(line.parse().expect("Bad range"));
     }
+
+    // Sort the ranges by start
+    ranges
+        .sort_by(|a, b| a.partial_cmp(b)
+        .expect("Unsortable range"));
+
+    let mut unique_ranges = vec![];
+
+    let remaining = ranges.into_iter().reduce(|mut acc, next| {
+
+        // Try to merge with the next item
+        match merge(&acc, &next) {
+
+            // Either no intersection, or merged
+            Some(standalone_or_merged) => {
+                acc = standalone_or_merged;
+            },
+
+            // No intersection, so it's a unique range
+            None => {
+                unique_ranges.push(acc);
+
+                // Start again from the next range
+                acc = next;
+            }
+        }
+
+        acc
+
+    }).unwrap_or(Range { start: 0, end: 0 });
+
+    // Add the remaining accumulator
+    unique_ranges.push(remaining);
+
+    println!("Ranges {:?}", unique_ranges);
+
+    // Sum up the ranges
+    let total = unique_ranges.iter()
+        .map(|r| r.end - r.start + 1)
+        .sum::<u64>();
+
+    println!("{}", total);
 }
