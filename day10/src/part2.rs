54e9f2216f98d0cc7ec1ed9e2158b850ac2c882f
Ret <folf@retsplin.es>
2025-12-14 22:51:11 +0000

    tidy up

--- day10/src/part2.rs
+++ day10/src/part2.rs
@@ -1,10 +1,9 @@
+use microlp::{ComparisonOp, LinearExpr, OptimizationDirection, Problem, Variable};
 use std::fmt;
 use std::fmt::Formatter;
 use std::str::FromStr;
-use microlp::{ComparisonOp, LinearExpr, OptimizationDirection, Problem, Variable};
 
-#[derive(Debug)]
-#[derive(Eq, Hash, PartialEq, Clone)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct Button(Vec<usize>, String);
 
 impl fmt::Display for Button {
@@ -20,32 +19,30 @@ impl FromStr for Button {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         // Strip the outer brackets and parse the inner CSV
         assert!(s.len() > 2, "Empty button list");
 
-        // Map each affected light to a usize then fold into a bitvec with those bits set
-        Ok(Button(s[1..s.len() - 1]
-                      .split(',')
-                      .map(|v| v.parse::<usize>().unwrap())
-                      .collect()
-                  , s.to_string()))
+        // Map each affected light to a usize
+        Ok(Button(
+            s[1..s.len() - 1]
+                .split(',')
+                .map(|v| v.parse::<usize>().unwrap())
+                .collect(),
+            s.to_string(),
+        ))
     }
 }
 
-#[derive(Debug)]
-#[derive(Eq, Hash, PartialEq, Clone)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct Machine {
-    lights: Vec<bool>,
     buttons: Vec<Button>,
-    joltages: Vec<i64>
+    joltages: Vec<i64>,
 }
 
 impl FromStr for Machine {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         // Split into spaced chunks
         // The first chunk will be the lights spec
         // The last chunk will be the joltages spec
@@ -55,32 +52,24 @@ impl FromStr for Machine {
         // Must be at least three
         assert!(parts.len() > 2, "Not enough machine descriptor parts");
 
-        // Convert the target lights into a list of booleans
-        let lights = parts[0].chars().fold(Vec::new(), |mut acc, c| {
-            match c {
-                '#' => acc.push(true),
-                '.' => acc.push(false),
-                _ => ()
-            }
-            acc
-        });
-
         // Parse the buttons
-        let buttons = parts[1..parts.len() - 1].iter()
-            .map(|part| { part.parse::<Button>().unwrap() } )
+        let buttons = parts[1..parts.len() - 1]
+            .iter()
+            .map(|part| part.parse::<Button>().unwrap())
             .collect();
 
         // Parse the Joltages list
         let joltages_spec = parts.last().unwrap();
         assert!(joltages_spec.len() > 2, "Invalid joltages spec");
-        let joltages = joltages_spec[1..joltages_spec.len() - 1].split(',')
+
+        let joltages = joltages_spec[1..joltages_spec.len() - 1]
+            .split(',')
             .map(|v| v.parse::<i64>().unwrap())
             .collect();
 
         Ok(Machine {
-            lights,
             buttons,
-            joltages
+            joltages,
         })
     }
 }
@@ -104,11 +93,12 @@ fn main() {
 
     // For each machine
     for (machine_index, machine) in machines.iter().enumerate() {
-
         let mut problem = Problem::new(OptimizationDirection::Minimize);
 
         // Create an integer variable for each button that can be pressed 0->infinity times
-        let button_vars: Vec<Variable> = machine.buttons.iter()
+        let button_vars: Vec<Variable> = machine
+            .buttons
+            .iter()
             .map(|_| problem.add_integer_var(1.0, (0, i32::MAX)))
             .collect();
 
@@ -117,7 +107,10 @@ fn main() {
             let mut expr = LinearExpr::empty();
 
             // For each button, if it contributes to this joltage, make the constraint dependent on it
-            machine.buttons.iter().enumerate()
+            machine
+                .buttons
+                .iter()
+                .enumerate()
                 .filter(|(_, button)| button.0.contains(&joltage_index))
                 .for_each(|(i, _)| expr.add(button_vars[i], 1.0));
 
@@ -129,7 +122,10 @@ fn main() {
 
         // Solve!
         let result = problem.solve().unwrap();
-        println!("Machine {machine_index} requires {}", result.objective().round());
+        println!(
+            "Machine {machine_index} requires {}",
+            result.objective().round()
+        );
         total += result.objective().round() as i32;
     }
 
